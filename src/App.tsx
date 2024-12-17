4482b70e3a1362a6e81870c69081de165a4253f0
abidoye korede <korexcoded@gmail.com>
2024-12-17 08:04:54 -0800

    learning typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Greet } from './components/Greet'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Greet name = 'Akorede' />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
