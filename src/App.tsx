83b8a4434a890227354e9696c9a2b2c8c6d9ff7a
abidoye korede <korexcoded@gmail.com>
2024-12-17 14:38:52 -0800

    lesson contd

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css'
 import { Greet } from './components/Greet'
+import { Person } from './components/Person'
 
 function App() {
-
+   const personName = {
+    first: 'Akorede',
+    last : 'Abidoye',
+   }
   return (
     <>
       <div>
-        <Greet name = 'Akorede' />
+        <Greet name = 'Akorede' MessageCount={50} isLoggedIn={true} />
+        <Person name = {personName} />
       </div>
       
     </>
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,10 +1,16 @@
 type GreetProps = {
-  name : string
-}
-export const Greet = (props : GreetProps ) => {  
+  name: string;
+  MessageCount: number;
+  isLoggedIn: boolean;
+};
+export const Greet = (props: GreetProps) => {
   return (
     <div>
-      <h2>Welcome {props.name}! You have 10 New messages</h2>
+      <h2>
+        {props.isLoggedIn
+          ? `Welcome ${props.name}! You have ${props.MessageCount} New messages`
+          : "Welcome Guest"}
+      </h2>
     </div>
-  )
-}
+  );
+};
